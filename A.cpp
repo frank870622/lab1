fe8cb40ea43fd8600c736b9fcadb258f5c7dfa84
frank870622 <frank870622@gmail.com>
2017-03-07 21:51:34 +0800

    2017 3/7

--- A.cpp
+++ A.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(int argc,char *argv[]){
-	int n = argv[1];
-	while(){
-	cout << n;
+	int n = atoi(argv[1]);
+	while(1 == 1){
+	cout << n << " ";
 	if(n == 1) return 0;
-	if(m/2 == 1)	n = 3*n+1;
+	if(n%2 == 1)	n = 3*n+1;
 	else n = n/2;
 	}
 	return 0;
