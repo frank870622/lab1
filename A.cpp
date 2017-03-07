8dceb5172f16709e3ffd30c1300b321c7727e88a
frank870622 <frank870622@gmail.com>
2017-03-07 21:42:55 +0800

    lab1.B

---
+++ A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc,char *argv[]){
+	int n = argv[1];
+	while(){
+	cout << n;
+	if(n == 1) return 0;
+	if(m/2 == 1)	n = 3*n+1;
+	else n = n/2;
+	}
+	return 0;
+}
+
