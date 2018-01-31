40c7e70d67d3524a19d3f8dd97bb0d4b69e9e8eb
frank870622 <frank870622@gmail.com>
2018-01-31 11:06:20 +0800

    2018/01/18 Hello World!!

--- A.cpp
+++ A.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
-
-int main(int argc,char *argv[]){
-	int n = atoi(argv[1]);
-	while(1 == 1){
-	cout << n << " ";
-	if(n == 1) return 0;
-	if(n%2 == 1)	n = 3*n+1;
-	else n = n/2;
+int main(){
+	int n;
+	cin >> n;
+	while ((cout << n << " ") && (n != 1)){
+	if(n%2 == 1)	n = 3 * n + 1;
+	else n = n / 2;
 	}
 	return 0;
 }
